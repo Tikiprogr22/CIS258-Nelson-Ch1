7e70a41284fb4549b78311f9b2cf71d79212dc1c
Tikiprogr22 <159641405+Tikiprogr22@users.noreply.github.com>
2024-06-20 18:07:57 -0400

    Add files via upload

---
+++ project02/project01-02.js
@@ -0,0 +1,19 @@
+/*  JavaScript 7th Edition
+    Chapter 1
+    Hands-On Project 1-2
+
+    Author: LaTysha Turner
+    Date:   06/20/2024
+
+    Filename: project01-02.js
+*/
+
+// Define variables for service name and service speed
+var service1Name = "Basic"
+var service2Name = "Express"
+var service3Name = "Extreme"
+var service4Name = "Ultimate"
+var service1Speed = "0 Mbps"
+var service2Speed = "100 Mbps"
+var service3Speed = "500 Mbps"
+var service4Speed = "1 Gig"
